34d9e546a6f652e78213ce4033cb94259ebf5da8
PythonSimon <8178778@qq.com>
2022-03-27 16:15:39 +0800

    Add images and BaseFrame

---
+++ Client/Controller/Base.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+
+import sys
+
+from wx import *
+
+
+class BaseFrame(Frame):
+
+    def __init__(self, size, title, icon, closingWarning="确认关闭？", window="default", backgroundColor="GREY"):
+        super(BaseFrame, self).__init__(parent=None, title=title, size=size, style=DEFAULT_FRAME_STYLE ^ MAXIMIZE_BOX)
+
+        self.closingWarning = closingWarning
+        self.warn = True
+
+        if window == "default":
+            self.panel = Panel(parent=self)
+        elif window == "split-v":
+            self.splitter = SplitterWindow(self, style=SP_3DSASH)
+            self.leftPanel = Panel(self.splitter)
+            self.rightPanel = Panel(self.splitter)
+            self.splitter.SplitVertically(self.leftPanel, self.rightPanel, self.GetSize()[0] / 2)
+            self.splitter.SetMinimumPaneSize(self.GetSize()[0] / 10)
+        elif window == "split-h":
+            self.splitter = SplitterWindow(self, style=SP_3DSASH)
+            self.topPanel = Panel(self.splitter)
+            self.bottomPanel = Panel(self.splitter)
+            self.splitter.SplitHorizontally(self.topPanel, self.bottomPanel, self.GetSize()[1] / 2)
+            self.splitter.SetMinimumPaneSize(self.GetSize()[1] / 10)
+
+        icon = Icon(icon, BITMAP_TYPE_PNG)
+
+        self.Center()
+        self.SetBackgroundColour(backgroundColor)
+        self.SetIcon(icon)
+        self.Bind(EVT_CLOSE, self.onClose)
+
+    def onClose(self, event):
+        if self.warn:
+            warnMd = MessageDialog(None, self.closingWarning, caption="提示", style=YES_NO | ICON_EXCLAMATION)
+
+            if warnMd.ShowModal() == ID_YES:
+                self.Destroy()
+                sys.exit(0)
+            else:
+                warnMd.Destroy()
+        else:
+            self.Destroy()
+            sys.exit(0)
+
+    def close(self, event=None):
+        self.Destroy()
+        sys.exit(0)
