09f26f822325784ae2c92ded51bbf9fc22e083cc
PythonSimon <8178778@qq.com>
2022-03-28 11:22:24 +0800

    Add Main(ClientMain) and edit LoginFrame

--- Client/ClientMain.py
+++ Client/ClientMain.py
@@ -1,1 +1,23 @@
 # coding=utf-8
+
+from wx import *
+
+from Controller.Login import LoginFrame
+
+
+class MainApp(App):
+
+    def OnInit(self):
+        mainFrame = LoginFrame()
+        mainFrame.Show()
+
+        return True
+
+    def OnExit(self):
+        return 0
+
+
+if __name__ == "__main__":
+    mainApp = MainApp()
+
+    mainApp.MainLoop()
--- Client/Controller/Login.py
+++ Client/Controller/Login.py
@@ -2,16 +2,21 @@
 
 from wx import *
 
-from Base import BaseFrame
+from .Base import BaseFrame
 
 
 class LoginFrame(BaseFrame):
 
     def __init__(self):
         super(LoginFrame, self).__init__(
-            (535, 420),
+            (455, 340),
             "秘信",
-            r"..\Resource\MainIcon",
+            r"Resource\MainIcon.png",
             warn=False,
             backgroundColor="rgb(239,252,255)"
         )
+
+        self.main()
+
+    def main(self):
+        pass
