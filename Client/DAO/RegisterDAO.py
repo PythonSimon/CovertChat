2e6eb5be313b57dd5b8dc67feca5a82dfb14d5c3
PythonSimon <8178778@qq.com>
2022-04-03 19:41:50 +0800

    Edit RegisterFrame

--- Client/DAO/RegisterDAO.py
+++ Client/DAO/RegisterDAO.py
@@ -1,5 +1,28 @@
 # coding=utf-8
 
 from pymysql import *
+from pymysql.err import *
 
-pass
+CONNECTION_fAILED = -1
+CREATE_FAILED = -2
+
+
+def register(email, name, password, password2):
+    try:
+        connection = connect(
+            host="",
+            user="",
+            password="",
+            database="CovertChat",
+            charset="utf8"
+        )
+
+        try:
+            pass
+        finally:
+            connection.close()
+    except OperationalError:
+        return CONNECTION_fAILED
+
+
+print(register("", "", "", ""))
