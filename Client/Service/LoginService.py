82a871ea82d837d3d25962c3c6db0234d58b873d
PythonSimon <8178778@qq.com>
2022-04-13 17:01:05 +0800

    Edit client's login request sender

--- Client/Service/LoginService.py
+++ Client/Service/LoginService.py
@@ -1,9 +1,18 @@
 # coding=utf-8
 
+from json import *
 from socket import *
 
 from Client.Configure import *
 
 
 def login(email, password):
-    pass
+    client = Client.client
+
+    requestJ = dumps({
+        "command": Command.LOGIN,
+        "email": email,
+        "password": password
+    })
+
+    client.sendto(requestJ.encode(), Server.SERVER_SERVICE_ADDRESS)
