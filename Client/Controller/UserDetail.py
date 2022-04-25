a862eb620bbfd03aa286e2833ad464c755444097
PythonSimon <8178778@qq.com>
2022-04-25 15:22:35 +0800

    Edit UserDetailFrame

--- Client/Controller/UserDetail.py
+++ Client/Controller/UserDetail.py
@@ -1,3 +1,19 @@
 # coding=utf-8
 
+from wx import *
 
+from Base import BaseFrame
+
+
+class UserDetailFrame(BaseFrame):
+
+    def __init__(self, parent, uid, email, name, pattern, password, *password2):
+        super(UserDetailFrame, self).__init__(
+            (400, 400),
+            "秘信",
+            r"..\Resource\MainIcon.png",
+            parent=parent,
+            warn=False,
+            backgroundColor="#F5F8F9",
+            style=DEFAULT_FRAME_STYLE ^ MAXIMIZE_BOX | FRAME_FLOAT_ON_PARENT
+        )
